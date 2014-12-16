a61a66e7775c29255056b5b96d14558ea46c4158
paul <Semenov_PV@krsk.irgups.ru>
2014-12-16 11:20:17 +0700

    Add Employees model

--- eq/models.py
+++ eq/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employees(models.Model):
+    surname = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
+    hired = models.DateField()
+    moved = models.DateField()
+
+    def __str__(self):
+        return self.surname + self.firstname
