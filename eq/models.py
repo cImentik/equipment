8f90a219b57b932fed4ff625b0f7ade0d2f744e7
paul <Semenov_PV@krsk.irgups.ru>
2014-12-16 16:25:56 +0700

    Add Anthropometry, Proportions model

--- eq/models.py
+++ eq/models.py
@@ -12,3 +12,20 @@ class Employees(models.Model):
 
     def __str__(self):
         return self.surname + ' ' + self.firstname
+
+
+class Anthropometry(models.Model):
+    head_around = models.SmallIntegerField()
+    face_around = models.SmallIntegerField()
+    growth = models.SmallIntegerField()
+    chest = models.SmallIntegerField()
+
+
+class Proportions(models.Model):
+    dress = models.SmallIntegerField()
+    pants = models.SmallIntegerField()
+    hat = models.SmallIntegerField()
+    gas_mask = models.SmallIntegerField()
+    glasses = models.SmallIntegerField()
+    height_size = models.SmallIntegerField()
+    shoes = models.SmallIntegerField()
