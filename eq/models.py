5cbcb36d9fbce5d52393ca32bba7f57ffc1665f3
paul <Semenov_PV@krsk.irgups.ru>
2014-12-17 17:12:03 +0700

    Add Ownership model

--- eq/models.py
+++ eq/models.py
@@ -1,5 +1,5 @@
+from datetime import date
 from django.db import models
-
 # Create your models here.
 
 
@@ -29,3 +29,32 @@ class Proportions(models.Model):
     glasses = models.SmallIntegerField()
     height_size = models.SmallIntegerField()
     shoes = models.SmallIntegerField()
+
+
+class TypeEquipments(models.Model):
+    group_name = models.CharField(max_length=20)
+
+
+class Constructions(models.Model):
+    group_name = models.ForeignKey(TypeEquipments)
+    constructons_name = models.CharField(max_length=20)
+
+
+class Equipments(models.Model):
+    type_eq = models.ForeignKey(TypeEquipments)
+    model_name = models.CharField(max_length=20)
+    manufacturer = models.CharField(max_length=20)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    life = models.FloatField(default=0.0)
+    #
+    construction = models.ForeignKey(Constructions)
+    shockproof = models.NullBooleanField(null=True, default=None)  #противоудар
+    insulation = models.NullBooleanField(null=True, default=None)  #шумоизоляция
+    #TODO: дописать модель
+
+
+class Ownership(models.Model):
+    equipment = models.ForeignKey(Equipments)
+    employees = models.ForeignKey(Employees)
+    #code_eq =
+    delivery = models.DateField(default=date.today())
