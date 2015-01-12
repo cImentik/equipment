e10c13ffcfa5d3cf4f49fa4bf3d83c62299461fd
paul <Semenov_PV@krsk.irgups.ru>
2015-01-12 14:25:54 +0700

    add expends.html file and expend_list on view method

--- eq/urls.py
+++ eq/urls.py
@@ -5,7 +5,7 @@ from eq import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^cart/(?P<employe_id>\d+)/', views.cart, name='cart'),
-    url(r'^empl/', views.empl, name='empl'),
+    url(r'^expends/', views.expend_list, name='expend_list'),
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.login, name='logout'),
 )
--- eq/views.py
+++ eq/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import Http404
-from django.http import HttpResponse
+from datetime import date
 from eq.models import Ownership, Equipments, Employees, TypeEquipments, Units
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 
@@ -19,20 +20,30 @@ def index(request, unit_id=1):
     return render(request, 'eq/i_list.html', context)
 
 
+#@permission_required('eq.', login_url="eq/login")
 def cart(request, employe_id):
     """Учётная карточка работника"""
+    user = auth.get_user(request)
+
     ownerships = Ownership.objects.filter(employees_id=employe_id)
     type_equipments = TypeEquipments.objects.all()
     if ownerships:
         employee = ownerships[0].employees
     else:
         raise Http404
-    context = {'ownerships': ownerships, 'employee': employee, 'type_equipments': type_equipments}
+    context = {
+        'ownerships': ownerships,
+        'employee': employee,
+        'type_equipments': type_equipments}
     return render(request, 'eq/cart.html', context)
 
 
-def empl(request):
-    return HttpResponse("Get me employees...")
+def expend_list(request, date_expend=date.today()):
+    """ Список СЗ на списание """
+    context = {
+        'date_expend': date_expend
+    }
+    return render_to_response('eq/expends.html', context)
 
 
 def login(request):
@@ -42,7 +53,7 @@ def login(request):
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         user = auth.authenticate(username=username, password=password)
-        if user is not None:
+        if user is not None and user.is_active:
             auth.login(request, user)
             return redirect('/')
         else:
