03e8ffabd94aff0f548b9404aa72bf35396ea9f9
paul <Semenov_PV@krsk.irgups.ru>
2015-09-30 16:48:45 +0700

    more new (

---
+++ get_sprav.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import csv
+
+s=[]
+
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
+
+with open('spravph.csv', newline='') as csvfile:
+    sreader = csv.reader(csvfile, delimiter=';')
+    for row in sreader:
+        c.execute("INSERT INTO t_table VALUES ('%s')" % row[1])
+        conn.commit()
+        #print(row[1])
+
+conn.close()
+
+
+# CREATE TABLE t_table
+# (
+#     name varchar(255) NOT NULL
+# )
